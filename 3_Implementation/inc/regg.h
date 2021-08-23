895ce94f0ead3fe19f3473ec26b42fcb4d443c64
parz14 <62294775+parz14@users.noreply.github.com>
2021-08-23 15:51:57 +0530

    Add files via upload

---
+++ 3_Implementation/inc/regg.h
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#ifndef _FUN_H
+#define _FUN_H
+
+
+int getColorCode(char colorCode[]);
+long getResistorValue( char colorCode[], int colorValue[], int r_value );
+
+
+    char colorCode[4];
+    int  colorValue[4];
+
