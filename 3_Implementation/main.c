15657f17d27a016342ea35725cb22f5e4fdc3eed
Sangeetha Nambiraj <67622262+sangeethanambiraj@users.noreply.github.com>
2021-08-23 16:43:50 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -6,16 +6,31 @@
 #include "inc/division.h"
 #include "inc/trace_width.h"
 #include "inc/DutyCycle.h"
-#include "regg.h"
+#include "inc/CircularConvolution.h"
 int main()
 {
     int num;
     printf("------------------------------------WELCOME-------------------------------------\n");
     printf("Select the type of calculation\n");
-    printf("1. Motor parameters\n2. Cable\n3. Star and Delta Conversions\n4. Laws\n5. Tensile strength\n6. Duty cycle\n7. Adders_Subtractors\n8. PCB Trace width\n9. Division\n10. Series and parallel\n11. Resistor Colour code\n");
+    printf("1. Motor parameters\n");
+    printf("2. Cable\n");
+    printf("3. Star Delta Conversion");
+    printf("4. Ohms law\n");
+    printf("5. KVL\n");
+    printf("6. KCL\n");
+    printf("7. Tensile calci\n");
+    printf("8. Dutycycle\n");
+    printf("9. Adder\n");
+    printf("10. Subtractor\n");
+    printf("11. PCB Trace Width\n");
+    printf("12.Division\n");
+    printf("13. Paralle\n");
+    printf("14. Series\n");
+    printf("15. Resistor colour code\n");
+    printf("16. Circular convolution\n");
     printf("Type : ");
     scanf("%d",&num);
-    int x,no,a;
+    int x,no;
     switch(num)
     {
         case 1:
@@ -33,87 +48,92 @@ int main()
         case 3:
         {
             printf("<-------------------------------------------Star and Delta Conversion------------------------------------------>\n");
-            printf("Select the component required\n-----------------------------------\n");
-            printf("1-Resistor\n2-Inductor\n3-Capacitor");
-            printf("\nSelect the component: ");
-            scanf("%d",&a);
-            starDeltaConversion(a);
+            starDeltaConversion();
             break;
         }
         case 4:
         {
-            printf("<------------------------------------------Basic laws---------------------------------------------\n");
-            scanf("%d",&no);
-            if(no==1)
+            printf("<------------------------------------------Ohms law---------------------------------------------\n");
             ohm();
-            else if(no=2)
-            kvl();
-            else if(no=3)
-            kcl();
-            else
-            printf("Invalid");
-            break;
             break;
         }
         case 5:
+        {
+            printf("<------------------------------------------KVL---------------------------------------------\n");
+            kvl();
+            break;
+        }
+        case 6:
+        {
+            printf("------------------------------------------KCL----------------------------------------\n");
+            kcl();
+            break;
+        }
+        case 7:
         {
             printf("------------------------------------------------tensileCalci----------------------------------\n");
             tscalci();
             break;
         }
-        case 6:
+        case 8:
         {
             printf("-----------------------------------------------DutyCycle---------------------------------\n");
             Sambit();
             break;
         }
-        case 7:
+        case 9:
         {
-            printf("------------------------------Adder and Subtractor---------------------------------\n");
-            scanf("%d",&x);
-            if(x==1)
+            printf("------------------------------Adder---------------------------------\n");
             adder();
-            else if(x==2)
-            subtractor();
-            else
-            printf("Invalid");
             break;
         }
-        case 8:
+        case 10:
+        {
+            printf("------------------------------Subtractor---------------------------------\n");
+            subtractor();
+            break;
+        }
+        case 11:
         {
             printf("--------------------------------------PCB trace width-----------------------------\n");
             trace_width_calculator();
             break;
         }
-        case 9: 
+        case 12: 
         {
             printf("---------------------------------------Division-------------------------------------\n");
             division();
             break;
         }
-        case 10:
+        case 13:
         {
-            printf("---------------------------------------Series and Parallel---------------------------\n");
-            int a;
-            scanf("%d",&a);
-            if(a==1)
-            series();
-            else if(a==2)
+            printf("---------------------------------------Parallel---------------------------\n");
             parallel();
-            else
-            printf("Invalid");
+            break;
         }
-        break;
-        case 11:
+        case 14:
+        {
+            printf("---------------------------------------Series---------------------------\n");
+            series();
+            break;
+        }
+        case 15:
         {
             printf("-------------------------------------Resistor Colour code------------------------------\n");
-            
             getColorCode(colorCode);
              r_value =  getResistorValue(colorCode, colorValue, r_value);
              printf("\n\nThe resistor with color code %c %c %c %c has a value of %d Ohms.\n",
              colorCode[0], colorCode[1], colorCode[2], colorCode[3], r_value);
         }
+
+        }
         break;
+        case 16:
+        {
+            printf("----------------------------------------Circular Convolution-----------------------------\n");
+            circular_convolution();
+            break;
+        }
         default:
         printf("Enter valid option");
    }
