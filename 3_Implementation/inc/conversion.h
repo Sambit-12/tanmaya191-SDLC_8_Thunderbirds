af0ec07778b31eb0ca38b005f9b3284321eddb8a
Sangeetha Nambiraj <67622262+sangeethanambiraj@users.noreply.github.com>
2021-08-23 11:22:15 +0530

    Add files via upload

---
+++ 3_Implementation/inc/conversion.h
@@ -0,0 +1,14 @@
+/**
+ * @file conversion.h
+ * @author Sangeetha Nambiraj
+ * @brief 
+ * @version 0.1
+ * @date 2021-08-22
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __CONVERSION_H__
+#define __CONVERSION_H__
+void starDeltaConversion();
+#endif
